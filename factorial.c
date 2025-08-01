65db93b9fcd25b16309b71c1375e3a530210782f
Abid Hasan <abidhasan2223334@gmail.com>
2025-08-01 23:09:00 +0600

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,27 @@
+/* 
+#include <stdio.h>
+int sum (int n){
+
+    int f=0;
+    int i;
+    for ( i=1; i<=n; i++) {
+
+   f=f+i; }
+
+    return f;
+}
+int main (){
+
+    int n;
+
+    printf("koto porjonto jog korte cao ? \n=  ");
+
+    scanf("%d", &n);
+
+    printf("sum of %d number is ", n);
+
+    printf(" %d", sum(n));
+
+    return 0;
+}
+    */
